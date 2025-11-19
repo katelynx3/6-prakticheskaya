f400e596c6826c27a6a599e50ed6655009989380
katelyn_x3 <saretx@icloud.com>
2025-11-19 11:25:35 +0300

    Delete 5.py

--- 5.py
+++
@@ -1,7 +0,0 @@
-x1, y1 = map(int, input("Введите координаты ладьи через пробел ").split())
-x2, y2 = map(int, input("Введите координаты хода ладьи через пробел ").split())
-if (abs(x2-x1) == abs(y2-y1) or (x1 == x2) or (y1 == y2)):
-    print('yes')
-else:
-    print('no')
-неанлеарпапрлп
